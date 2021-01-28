8f6fc138e704a9aeb2bb38960cf53f4360f6d6ba
ap-pauloafonso <ap.pauloafonso@gmail.com>
2021-01-27 23:03:08 -0300

    initial contract

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/grpc-queue/grpc-queue/internal/queueproto"
@@ -12,4 +13,17 @@ type Server struct{}
 func (s *Server) SayHello(context context.Context, in *queueproto.Message) (*queueproto.Message, error) {
 	fmt.Println("Received: " + in.Body)
 	return &queueproto.Message{Body: "hi " + in.Body}, nil
+}
+
+func (s *Server) CreateStream(context context.Context, in *queueproto.CreateStreamMessage) (*queueproto.ResponseMessage, error) {
+	return nil, errors.New("CreateStream not implemented yet")
+}
+func (s *Server) DetailStream(context context.Context, in *queueproto.DetailStreamMessage) (*queueproto.DetailStreamResponseMessage, error) {
+	return nil, errors.New("DetailStream not implemented yet")
+}
+func (s *Server) Enqueue(context context.Context, in *queueproto.EnqueueMessage) (*queueproto.ResponseMessage, error) {
+	return nil, errors.New("Enqueue not implemented yet")
+}
+func (s *Server) Dequeue(context context.Context, in *queueproto.DequeueMessage) (*queueproto.DequeueResponseMessage, error) {
+	return nil, errors.New("Dequeue not implemented yet")
 }
