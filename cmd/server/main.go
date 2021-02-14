eb961e4b36ecb42f7951b9a9a900f24ddea9bc80
ap-pauloafonso <ap.pauloafonso@gmail.com>
2021-02-14 18:45:19 -0300

    several good  changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net"
+	"strconv"
 
-	"github.com/grpc-queue/grpc-queue/internal/queueproto"
 	"github.com/grpc-queue/grpc-queue/internal/server"
+	"github.com/grpc-queue/grpc-queue/pkg/grpc/v1/queue"
 	"google.golang.org/grpc"
+)
+
+const (
+	port = 9000
 )
 
 func main() {
 
-	lis, err := net.Listen("tcp", ":9000")
+	lis, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	queueproto.RegisterQueueServiceServer(grpcServer, &server.Server{})
+
+	queue.RegisterQueueServiceServer(grpcServer, &server.Server{})
+
+	log.Printf("grpc server started listening on port %d", port)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,27 +3,24 @@ package server
 import (
 	"context"
 	"errors"
-	"fmt"
 
-	"github.com/grpc-queue/grpc-queue/internal/queueproto"
+	"github.com/grpc-queue/grpc-queue/pkg/grpc/v1/queue"
 )
 
-type Server struct{}
-
-func (s *Server) SayHello(context context.Context, in *queueproto.Message) (*queueproto.Message, error) {
-	fmt.Println("Received: " + in.Body)
-	return &queueproto.Message{Body: "hi " + in.Body}, nil
+type Server struct {
+	*queue.UnimplementedQueueServiceServer
 }
 
-func (s *Server) CreateStream(context context.Context, in *queueproto.CreateStreamMessage) (*queueproto.ResponseMessage, error) {
+func (s *Server) CreateStream(ctx context.Context, request *queue.CreateStreamRequest) (*queue.CreateStreamResponse, error) {
 	return nil, errors.New("CreateStream not implemented yet")
 }
-func (s *Server) DetailStream(context context.Context, in *queueproto.DetailStreamMessage) (*queueproto.DetailStreamResponseMessage, error) {
-	return nil, errors.New("DetailStream not implemented yet")
+func (s *Server) GetStreams(ctx context.Context, request *queue.GetStreamsRequest) (*queue.GetStreamsResponse, error) {
+	return nil, errors.New("GetStreams not implemented yet")
 }
-func (s *Server) Enqueue(context context.Context, in *queueproto.EnqueueMessage) (*queueproto.ResponseMessage, error) {
-	return nil, errors.New("Enqueue not implemented yet")
+
+func (s *Server) Push(ctx context.Context, request *queue.PushItemRequest) (*queue.PushItemResponse, error) {
+	return nil, errors.New("Push not implemented yet")
 }
-func (s *Server) Dequeue(context context.Context, in *queueproto.DequeueMessage) (*queueproto.DequeueResponseMessage, error) {
-	return nil, errors.New("Dequeue not implemented yet")
+func (s *Server) Pop(ctx context.Context, request *queue.PopItemRequest) (*queue.PopItemResponse, error) {
+	return nil, errors.New("Pop not implemented yet")
 }
