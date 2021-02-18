b2648594904ccb2ab686c38547aae7187824b0e8
ap-pauloafonso <ap.pauloafonso@gmail.com>
2021-02-18 18:13:24 -0300

    client pkg and makefile

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/grpc-queue/grpc-queue/pkg/grpc/v1/queue"
@@ -9,74 +10,49 @@ import (
 )
 
 type client struct {
-	address string
+	conn *grpc.ClientConn
 }
 
-func NewClient(address string) *client {
-	return &client{address: address}
+func NewClient(c *grpc.ClientConn) *client {
+	return &client{conn: c}
 }
 
-func (c *client) CreateStream(req *queue.CreateStreamRequest) (resp *queue.CreateStreamResponse) {
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(c.address, grpc.WithInsecure())
+func (c *client) CreateStream(ctx context.Context, req *queue.CreateStreamRequest) (resp *queue.CreateStreamResponse, err error) {
+	service := queue.NewQueueServiceClient(c.conn)
+	response, err := service.CreateStream(ctx, req)
 	if err != nil {
-		log.Fatalf("did not connect: %s", err)
-	}
-	defer conn.Close()
-
-	service := queue.NewQueueServiceClient(conn)
-	response, err := service.CreateStream(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error when calling CreateStream: %s", err)
+		return nil, fmt.Errorf("%s: %w", "Error when calling CreateStream", err)
 	}
 	log.Printf("Response from server: %+v", response)
-	return response
+	return response, nil
 }
-func (c *client) GetStreams(req *queue.GetStreamsRequest) (resp *queue.GetStreamsResponse) {
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(c.address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %s", err)
-	}
-	defer conn.Close()
+func (c *client) GetStreams(ctx context.Context, req *queue.GetStreamsRequest) (resp *queue.GetStreamsResponse, err error) {
 
-	service := queue.NewQueueServiceClient(conn)
-	response, err := service.GetStreams(context.Background(), req)
+	service := queue.NewQueueServiceClient(c.conn)
+	response, err := service.GetStreams(ctx, req)
 	if err != nil {
-		log.Fatalf("Error when calling GetStreams: %s", err)
+		return nil, fmt.Errorf("%s: %w", "Error when calling GetStreams", err)
 	}
 	log.Printf("Response from server: %+v", response)
-	return response
+	return response, nil
 }
-func (c *client) Push(req *queue.PushItemRequest) (resp *queue.PushItemResponse) {
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(c.address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %s", err)
-	}
-	defer conn.Close()
+func (c *client) Push(ctx context.Context, req *queue.PushItemRequest) (resp *queue.PushItemResponse, err error) {
 
-	service := queue.NewQueueServiceClient(conn)
-	response, err := service.Push(context.Background(), req)
+	service := queue.NewQueueServiceClient(c.conn)
+	response, err := service.Push(ctx, req)
 	if err != nil {
 		log.Fatalf("Error when calling Push: %s", err)
+		return nil, fmt.Errorf("%s: %w", "Error when calling Push", err)
 	}
 	log.Printf("Response from server: %+v", response)
-	return response
+	return response, nil
 }
-func (c *client) Pop(req *queue.PopItemRequest) (resp *queue.PopItemResponse) {
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(c.address, grpc.WithInsecure())
+func (c *client) Pop(ctx context.Context, req *queue.PopItemRequest) (resp *queue.PopItemResponse, err error) {
+	service := queue.NewQueueServiceClient(c.conn)
+	response, err := service.Pop(ctx, req)
 	if err != nil {
-		log.Fatalf("did not connect: %s", err)
-	}
-	defer conn.Close()
-
-	service := queue.NewQueueServiceClient(conn)
-	response, err := service.Pop(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error when calling Pop: %s", err)
+		return nil, fmt.Errorf("%s: %w", "Error when calling Pop", err)
 	}
 	log.Printf("Response from server: %+v", response)
-	return response
+	return response, nil
 }
